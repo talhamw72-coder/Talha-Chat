3c77aef6569230e32f656a4b6394d054261b2888
talhamw72-coder <talhamw72@gmail.com>
2026-03-12 08:45:46 +0600

    Update index.js for proper AI handling

---
+++ LeftSidebar.js
@@ -0,0 +1,23 @@
+function LeftSidebar(){
+
+return(
+
+<div className="sidebar">
+
+<ul>
+
+<li>Profile</li>
+<li>Friends</li>
+<li>Groups</li>
+<li>Marketplace</li>
+<li>Memories</li>
+
+</ul>
+
+</div>
+
+)
+
+}
+
+export default LeftSidebar
