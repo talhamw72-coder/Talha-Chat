e07a3150e619d3ba7c94601c3e09577410b5218e
talhamw72-coder <talhamw72@gmail.com>
2026-03-12 08:47:35 +0600

    Update index.js for proper AI handling

---
+++ PostCard.js
@@ -0,0 +1,25 @@
+function PostCard(){
+
+return(
+
+<div className="post">
+
+<h4>Talha</h4>
+
+<p>Hello this is my first post</p>
+
+<div>
+
+<button>Like</button>
+<button>Comment</button>
+<button>Share</button>
+
+</div>
+
+</div>
+
+)
+
+}
+
+export default PostCard
