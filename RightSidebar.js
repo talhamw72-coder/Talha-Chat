ebc9cb4ef328115b78811ed44c69ff60a3240f10
talhamw72-coder <talhamw72@gmail.com>
2026-03-12 08:46:27 +0600

    Update index.js for proper AI handling

---
+++ RightSidebar.js
@@ -0,0 +1,23 @@
+function RightSidebar(){
+
+return(
+
+<div className="rightbar">
+
+<h3>Online Friends</h3>
+
+<ul>
+
+<li>Rahim</li>
+<li>Karim</li>
+<li>Hasan</li>
+
+</ul>
+
+</div>
+
+)
+
+}
+
+export default RightSidebar
