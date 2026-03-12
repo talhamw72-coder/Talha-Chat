02d8a34a7391ead84e87c09a2835d4d14c99f756
talhamw72-coder <talhamw72@gmail.com>
2026-03-12 08:45:02 +0600

    Update index.js for proper AI handling

---
+++ Navbar.js
@@ -0,0 +1,27 @@
+function Navbar(){
+
+return(
+
+<div className="navbar">
+
+<h2>Talha</h2>
+
+<input
+placeholder="Search Talha"
+/>
+
+<div>
+
+<span>Home</span>
+<span>Messages</span>
+<span>Profile</span>
+
+</div>
+
+</div>
+
+)
+
+}
+
+export default Navbar
