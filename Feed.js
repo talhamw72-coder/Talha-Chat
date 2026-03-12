60f685ca7d3652eba8c97252b93706ef44d8a268
talhamw72-coder <talhamw72@gmail.com>
2026-03-12 08:47:02 +0600

    Update index.js for proper AI handling

---
+++ Feed.js
@@ -0,0 +1,22 @@
+import CreatePost from "./CreatePost"
+import PostCard from "./PostCard"
+
+function Feed(){
+
+return(
+
+<div className="feed">
+
+<CreatePost/>
+
+<PostCard/>
+
+<PostCard/>
+
+</div>
+
+)
+
+}
+
+export default Feed
